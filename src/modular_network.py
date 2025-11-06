13b296127be8b7550725bdfede32ecf5d724edd7
winoo19 <danielsanchezs1903@gmail.com>
2025-11-06 19:04:51 +0000

    [refactor] Modularized code

---
+++ src/modular_network.py
@@ -0,0 +1,309 @@
+from src.iznetwork import IzNetwork
+import numpy as np
+
+
+class ModularNetworkGenerator:
+    """
+    Class to generate modular Izhikevich networks with specified rewiring
+    probability p.
+    """
+
+    def __init__(self, p: float, params: dict):
+        """
+        Initializes the Modular Network Generator with rewiring probability p
+        and network parameters.
+
+        Parameters:
+        p      -- Rewiring probability for E-E connections.
+        params -- Dictionary containing network parameters.
+        """
+        self.p = p
+        self.params = params
+
+        # Unpack base parameters
+        self.NUMBER_OF_MODULES = self.params["NUMBER_OF_MODULES"]
+        self.EXCITATORY_PER_MODULE = self.params["EXCITATORY_PER_MODULE"]
+        self.INHIBITORY_NEURONS = self.params["INHIBITORY_NEURONS"]
+        self.CONNECTIONS_PER_MODULE = self.params["CONNECTIONS_PER_MODULE"]
+
+        # Calculate derived parameters inside the function
+        self.TOTAL_EXCITATORY_NEURONS = (
+            self.NUMBER_OF_MODULES * self.EXCITATORY_PER_MODULE
+        )
+        self.TOTAL_NEURONS = self.TOTAL_EXCITATORY_NEURONS + self.INHIBITORY_NEURONS
+
+    def _generate_neuron_parameters(
+        self,
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Initializes the Izhikevich neuron parameters for the entire network.
+        Excitatory and Inhibitory neurons have different parameters.
+
+        Returns:
+        a, b, c, d -- np.arrays of size TOTAL_NEURONS containing the parameters for each neuron.
+        """
+        # Izhikevich Neuron Parameters
+        a = np.zeros(self.TOTAL_NEURONS)
+        b = np.zeros(self.TOTAL_NEURONS)
+        c = np.zeros(self.TOTAL_NEURONS)
+        d = np.zeros(self.TOTAL_NEURONS)
+
+        # Set up Izhikevich Parameters for Excitatory Neurons
+        a[: self.TOTAL_EXCITATORY_NEURONS] = self.params["excitatory_iz_neuron"]["a"]
+        b[: self.TOTAL_EXCITATORY_NEURONS] = self.params["excitatory_iz_neuron"]["b"]
+        c[: self.TOTAL_EXCITATORY_NEURONS] = self.params["excitatory_iz_neuron"]["c"]
+        d[: self.TOTAL_EXCITATORY_NEURONS] = self.params["excitatory_iz_neuron"]["d"]
+
+        # Set up Izhikevich Parameters for Inhibitory Neurons
+        a[self.TOTAL_EXCITATORY_NEURONS :] = self.params["inhibitory_iz_neuron"]["a"]
+        b[self.TOTAL_EXCITATORY_NEURONS :] = self.params["inhibitory_iz_neuron"]["b"]
+        c[self.TOTAL_EXCITATORY_NEURONS :] = self.params["inhibitory_iz_neuron"]["c"]
+        d[self.TOTAL_EXCITATORY_NEURONS :] = self.params["inhibitory_iz_neuron"]["d"]
+
+        return a, b, c, d
+
+    def _generate_ee_connections(self) -> list[tuple[int, int]]:
+        """
+        Generates all Intra-Modular E-E connections.
+        This is an implementation of Topic 8, Slide 3.
+
+        Returns:
+        ee_connections_list -- List of tuples (src_node, tgt_node) for all created E-E connections.
+        """
+        ee_connections_list = []  # List to store connections for rewiring
+
+        for i_mod in range(self.NUMBER_OF_MODULES):
+            mod_start = i_mod * self.EXCITATORY_PER_MODULE
+            mod_end = (i_mod + 1) * self.EXCITATORY_PER_MODULE
+
+            connection_count = 0
+            while connection_count < self.CONNECTIONS_PER_MODULE:
+                src_node = np.random.randint(mod_start, mod_end)
+                tgt_node = np.random.randint(mod_start, mod_end)
+
+                if (
+                    src_node == tgt_node or self.W[src_node, tgt_node] != 0
+                ):  # Check for self or existing
+                    continue
+
+                # Create the E-E connection
+                # Numbers from Topic 9 Slide 12
+                scaling_factor = 17
+                weight = 1
+                min_delay = 1
+                max_delay = 20
+                self.W[src_node, tgt_node] = scaling_factor * weight
+                self.D[src_node, tgt_node] = np.random.randint(min_delay, max_delay + 1)
+
+                # Store this connection to be (potentially) rewired
+                ee_connections_list.append((src_node, tgt_node))
+                connection_count += 1
+
+        return ee_connections_list
+
+    def _rewire_ee_connections(self, ee_connections_list: list[tuple[int, int]]) -> int:
+        """
+        Rewires E-E connections based on the rewiring probability p.
+        Returns the number of connections that were rewired.
+
+        Parameters:
+        ee_connections_list -- List of tuples (src_node, tgt_node) for all created E-E connections.
+
+        Returns:
+        rewired_count -- Number of connections that were successfully rewired.
+        """
+
+        rewired_count = 0
+
+        for src_node, tgt_node in ee_connections_list:
+            # Decide if this connection should be rewired
+            if np.random.rand() < self.p:
+
+                # --- Find a new Inter-modular connection ---
+                new_tgt_found = False
+                i_mod = src_node // self.EXCITATORY_PER_MODULE  # Module of the source
+
+                for _ in range(100):  # 100 attempts to find a new target
+                    new_tgt = np.random.randint(0, self.TOTAL_EXCITATORY_NEURONS)
+                    new_mod_idx = new_tgt // self.EXCITATORY_PER_MODULE
+
+                    # Check validity:
+                    # 1. Not a self-connection
+                    # 2. Not in the same module as the source
+                    # 3. Connection doesn't already exist
+                    if (
+                        src_node != new_tgt
+                        and new_mod_idx != i_mod
+                        and self.W[src_node, new_tgt] == 0
+                    ):
+
+                        # Create the new inter-modular connection
+                        self.W[src_node, new_tgt] = 17.0  # Set E-E weight
+                        self.D[src_node, new_tgt] = np.random.randint(
+                            1, 21
+                        )  # Set E-E delay
+
+                        # Delete the old intra-modular connection
+                        self.W[src_node, tgt_node] = 0.0
+                        self.D[src_node, tgt_node] = 1  # Reset delay to default
+
+                        rewired_count += 1
+                        new_tgt_found = True
+                        break  # Found a valid new target
+
+                # If we failed to find a new target after 100 tries,
+                # we just leave the original intra-modular connection.
+                # (The rewire attempt fails, but total connections remain)
+                if not new_tgt_found:
+                    # This print is for debugging, can be removed
+                    # print(f"    Warning: Could not find rewire target for {src} -> {tgt}")
+                    pass
+
+        return rewired_count
+
+    def _create_ei_connections(self):
+        """
+        Creates Focal E-I connections.
+        """
+        # 200 inh neurons, 8 modules -> 25 inh neurons per module //TODO:maybe random
+        inh_per_module = self.INHIBITORY_NEURONS // self.NUMBER_OF_MODULES
+        for i_inh in range(self.INHIBITORY_NEURONS):
+            inh_neuron_idx = self.TOTAL_EXCITATORY_NEURONS + i_inh
+            # Find which module this inh neuron belongs to
+            module_idx = i_inh // inh_per_module
+
+            mod_start = module_idx * self.EXCITATORY_PER_MODULE
+            mod_end = (module_idx + 1) * self.EXCITATORY_PER_MODULE
+
+            # Randomly choose 4 unique source neurons from this module
+            # (as per Topic 9)
+            src_list = np.random.choice(range(mod_start, mod_end), 4, replace=False)
+
+            for src_node in src_list:
+                # Set weight (rand(0,1) * 50)
+                self.W[src_node, inh_neuron_idx] = np.random.rand() * 50.0  # <-- Use W
+                # Set delay (1ms)
+                self.D[src_node, inh_neuron_idx] = 1
+
+    def _create_ie_connections(self):
+        """
+        Creates Focal I-E connections as specified in Topic 9, slide 1.
+        """
+        for src_inh_idx in range(self.TOTAL_EXCITATORY_NEURONS, self.TOTAL_NEURONS):
+            for tgt_exc_idx in range(self.TOTAL_EXCITATORY_NEURONS):
+                # Set weight (rand(-1,0) * 2)
+                self.W[src_inh_idx, tgt_exc_idx] = (np.random.rand() - 1.0) * 2.0
+                # Set delay (1ms)
+                self.D[src_inh_idx, tgt_exc_idx] = 1
+
+    def _create_ii_connections(self):
+        """
+        Creates Focal I-I connections as specified in Topic 9, slide 1.
+        """
+        for src_inh_idx in range(self.TOTAL_EXCITATORY_NEURONS, self.TOTAL_NEURONS):
+            for tgt_inh_idx in range(self.TOTAL_EXCITATORY_NEURONS, self.TOTAL_NEURONS):
+                if src_inh_idx != tgt_inh_idx:
+                    # Set weight (Topic 9, Slide 4: rand(-1,0) * 1)
+                    self.W[src_inh_idx, tgt_inh_idx] = np.random.rand() - 1.0
+                    # Set delay (Topic 9, Slide 4: 1ms)
+                    self.D[src_inh_idx, tgt_inh_idx] = 1
+
+    def generate_modular_network(self) -> IzNetwork:
+        """
+        Generates a modular Izhikevich network for a given rewiring probability p.
+
+        Returns:
+        network -- An instance of IzNetwork configured with the generated connections.
+        """
+
+        # Connectivity and Delay Matrices
+        self.W = np.zeros((self.TOTAL_NEURONS, self.TOTAL_NEURONS))
+        # Initialize all delays to 1ms (since it is the default for
+        # E-I, I-E and I-I connections)
+        # E-E delays will be overwritten
+        self.D = np.ones((self.TOTAL_NEURONS, self.TOTAL_NEURONS), dtype=int)
+
+        # === Generate E-E Connections and Rewire ===
+        # This is an implementation of the algorithm described in Lecture 4 Topic 8
+        # using the weight, scaling factor and delay parameters from Topic 9
+
+        print(f"Generating E-E connections for p={self.p}...")
+        print(
+            f"Creating {self.CONNECTIONS_PER_MODULE * self.NUMBER_OF_MODULES} intra-modular connections"
+        )
+        ee_connections_list = self._generate_ee_connections()
+
+        total_ee_conns = np.count_nonzero(
+            self.W[: self.TOTAL_EXCITATORY_NEURONS, : self.TOTAL_EXCITATORY_NEURONS]
+        )
+        print(f"{total_ee_conns} E-E connections created.")
+
+        # --- Step 2: Rewire connections based on p ---
+        # (This is an implementation of Topic 8, Slide 4)
+        print(f"Step 2: Rewiring connections with p={self.p}...")
+        rewired_count = self._rewire_ee_connections(ee_connections_list)
+
+        print(f"{rewired_count} connections were rewired.")
+
+        # === 3. Generate Inhibitory Connections (Topic 9) ===
+        print("Generating inhibitory connections...")
+
+        # A. E-I Connections (Focal)
+        self._create_ei_connections()
+
+        # B. I-E Connections (Diffuse)
+        self._create_ie_connections()
+
+        # C. I-I Connections (Diffuse)
+        self._create_ii_connections()
+
+        # === 4. Finalizing the Network ===
+        print("Configuring IzNetwork instance...")
+
+        # Dmax is 20ms from the E-E connections (Topic 9, Slide 4)
+        network = IzNetwork(N=self.TOTAL_NEURONS, Dmax=20)
+        network.setParameters(*self._generate_neuron_parameters())
+        network.setWeights(self.W)
+        network.setDelays(self.D)
+
+        print(f"Network for p={self.p} generated.")
+        return network
+
+
+def main():
+    # === Global Parameters ===
+    network_params = {
+        # Modular Networks Experimental Setup (from Lecture 4 Topic 9)
+        "NUMBER_OF_MODULES": 8,
+        "EXCITATORY_PER_MODULE": 100,
+        "INHIBITORY_NEURONS": 200,
+        "CONNECTIONS_PER_MODULE": 1000,
+        # Excitatory Izhikevich Neuron parameters (from Lecture 2 Topic 4)
+        "excitatory_iz_neuron": {"a": 0.02, "b": 0.2, "c": -65.0, "d": 8.0},
+        # Inhibitory Izhikevich Neuron parameters (from Lecture 2 Topic 4)
+        "inhibitory_iz_neuron": {"a": 0.02, "b": 0.25, "c": -65.0, "d": 2.0},
+    }
+
+    # The p values specified in coursework
+    P_VALUES = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+    # Dictionary to store the generated networks
+    networks = {}
+
+    # === 2. Main Loop ===
+    print("--- Starting network generation ---")
+
+    for p in P_VALUES:
+        print(f"--- Generating network for p = {p} ---")
+        generator = ModularNetworkGenerator(p, network_params)
+
+        net = generator.generate_modular_network()
+        networks[p] = net
+
+        print(f"--- Finished p = {p} ---")
+
+    print(f"--- All 6 networks generated ---")
+
+
+if __name__ == "__main__":
+    main()
